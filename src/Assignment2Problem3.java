58071bdc60e22bde7383c82be844255721c42170
4UR3L10 <aureliomartinezdros@outlook.com>
2019-10-10 14:30:40 -0400

    Almost Done
    Simplify when is bigger the one popped from Stack One

--- src/Assignment2Problem3.java
+++ src/Assignment2Problem3.java
@@ -17,21 +17,41 @@ public class Assignment2Problem3
 	stackOne.push(4);
 	stackOne.push(11);
 	stackOne.push(4);
+	System.out.println(stackOne);
 	
 	// TempVars.
 	int tempOne = 0;
 	int tempTwo = 0;
-	
-	
-	
-	
+		
+	// While StackTwo 
+	while(!stackOne.isEmpty())
+	{
+	    tempOne = stackOne.pop();
+	    
+	    // Push value with no comparison.
+	    if(stackTwo.isEmpty())
+	    {
+		stackTwo.push(tempOne);
+	    }
+	    // Else compare with the one is already on the stack.
+	    else
+	    {
+		// If Popped from StackOne is bigger than last inserted in StackTwo.
+		// Pop from StackTwo and push the smaller value.
+		if(tempOne > stackTwo.peek())
+		{
+		    //tempTwo = stackTwo.pop();
+		    stackOne.push(stackTwo.pop());
+		    stackTwo.push(tempOne);
+		}
+		// If Popped from StackOne is smaller or equal than last inserted in StackTwo.
+		else
+		{
+		    stackTwo.push(tempOne);
+		}
+	    }
+	}
+	System.out.println(stackTwo);
 	System.out.println(stackOne);
-	
-	
-    }
-    
-    public static int Compare(int tempOne, int tempTwo)
-    {
-	return 0;
-    }
+    }       
 }
