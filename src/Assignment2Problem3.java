eb2c33d2471f179a93a5afb6ee6cc8afaeda442d
4UR3L10 <aureliomartinezdros@outlook.com>
2019-10-10 13:08:47 -0400

    Initial commit

---
+++ src/Assignment2Problem3.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class Assignment2Problem3
+{
+
+    public static void main(String[] args)
+    {
+	Stack<Integer> stack = new Stack<Integer>();
+	
+	stack.push(5);
+	System.out.println(stack.peek());
+    }
+
+}
